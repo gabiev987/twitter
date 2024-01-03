227999d9028aa0c141fb640a324abf633c9da3c2
Gabi <gabi_gabiev@mail.ru>
2024-01-03 07:04:15 +0300

    Добавил роль админа. Добавил возможность редактировать пользователей.

--- src/main/java/com/gabiev/twitter/domain/Role.java
+++ src/main/java/com/gabiev/twitter/domain/Role.java
@@ -3,7 +3,8 @@ package com.gabiev.twitter.domain;
 import org.springframework.security.core.GrantedAuthority;
 
 public enum Role implements GrantedAuthority {
-    USER;
+    USER,
+    ADMIN;
 
 
     @Override
